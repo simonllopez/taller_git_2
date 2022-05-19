7a0c4be9f105fb0653b66bb441b5941abb1708d8
simon <simonllopez@gmail.com>
2022-05-19 10:32:33 +0200

    Creacion de funcion en clase
    Se ha cambiado la funcion externa por una interna dentro de la clase de punto2d

--- script.py
+++ script.py
@@ -6,13 +6,12 @@ class Punto2d:
         self.x = x
         self.y = y
 
-
-def get_distance_two_points(x1, y1, x, y):
-    dx = (x1 - x)**2
-    dy = (y1 - y)**2
-    return math.sqrt(dx + dy)
+    def get_distance(self, x, y):
+        dx = (self.x - x)**2
+        dy = (self.y - y)**2
+        return math.sqrt(dx + dy)
 
 
 mi_punto = Punto2d(4, 0)
 
-print(get_distance_two_points(mi_punto.x, mi_punto.y, 2, 2))
+print(mi_punto.get_distance(2, 2))
